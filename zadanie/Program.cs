1f2cbad8c03ad3c2ab9aa02c080d6003174f016c
calemb79 <calemb79@gmail.com>
2023-12-01 23:30:58 +0100

    zadanie

---
+++ zadanie/Program.cs
@@ -0,0 +1,16 @@
+﻿var name = "Ewa";
+var sex = "Kobieta";
+var sexm = "Mężczyzna";
+var age = 30;
+//var age2 = 30;
+//var age3 = 33;
+
+if (sex == "Kobieta" && age - 1 < 30)
+    Console.WriteLine("Kobieta poniżej 30 lat");
+if (name == "Ewa" && age + 3 == 33)
+    Console.WriteLine("Ewa, lat 33");
+if (age - 13 < 18 && sexm != "Kobieta")
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+if (age - 13 > 18 && sexm == "Mężczyzna")
+    Console.WriteLine("Pełnoletni Mężczyzna");
+else Console.WriteLine("*KONIEC*");
