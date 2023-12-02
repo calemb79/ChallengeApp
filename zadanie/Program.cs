9e912abce38e921b946af6a369201afff3f4e055
calemb79 <calemb79@gmail.com>
2023-12-02 23:04:05 +0100

    dzien4

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -2,19 +2,19 @@
 
 var name = "Ewa";
 var sex = "K";
-var age = 50;
+var age = 33;
 
 if (sex == "K")
 {
     if (age == 33 && name == "Ewa")
         Console.WriteLine("Ewa, lat 33");
-    else if (age > 18)
-        Console.WriteLine("Pełnoletnia kobieta");
-    if (age < 30)
+    else if (age < 30)
         Console.WriteLine("Kobieta poniżej 30 Lat");
-    else if (age < 18)
+    if (age < 18)
         Console.WriteLine("Niepełnoletnia kobieta");
-    
+    if (age > 18)
+        Console.WriteLine("Pełnoletnia kobieta");
+
 }
 else if (sex !="K")
 {
