8367f4c6f86c9a64bd88bfed5a13bb1fe4e55428
calemb79 <calemb79@gmail.com>
2023-12-03 16:25:05 +0100

    DZIEN4-2

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -1,27 +1,38 @@
 ﻿using System.ComponentModel.Design;
 
 var name = "Ewa";
-var sex = "K";
-var age = 33;
+var sex = "M";
+var age = 30;
 
 if (sex == "K")
 {
     if (age == 33 && name == "Ewa")
+    {
         Console.WriteLine("Ewa, lat 33");
+    }
     else if (age < 30)
-        Console.WriteLine("Kobieta poniżej 30 Lat");
-    if (age < 18)
-        Console.WriteLine("Niepełnoletnia kobieta");
-    if (age > 18)
-        Console.WriteLine("Pełnoletnia kobieta");
+    { 
+        Console.WriteLine("Kobieta poniżej 30 Lat"); 
+    }
+    else
+    {
+        Console.WriteLine("inna kobieta");
+    }
+
 
 }
 else if (sex !="K")
 {
     if (age < 18)
+    {
         Console.WriteLine("Niepełnoletni Mężczyna");
-    else if(age > 18) 
+    }
+    else if (age > 18)
+    {
         Console.WriteLine("Pełnoletni mężczyna");
+    }
+    
+        
 }
 
 
