bb476a3ca67809dac5d8ebea850e979f87447cbb
calemb79 <calemb79@gmail.com>
2023-12-04 23:06:16 +0100

    dzien5

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -1,10 +1,10 @@
-﻿using System.ComponentModel.Design;
+﻿/*using System.ComponentModel.Design;
 
 var name = "Ewa";
-var sex = "M";
+bool sex = true;
 var age = 30;
 
-if (sex == "K")
+if (sex == true)
 {
     if (age == 33 && name == "Ewa")
     {
@@ -21,7 +21,7 @@ if (sex == "K")
 
 
 }
-else if (sex !="K")
+else if (sex !=true)
 {
     if (age < 18)
     {
@@ -34,7 +34,96 @@ else if (sex !="K")
     
         
 }
+*/
 
 
+int number = 1712154;
+string change = number.ToString();
+char[] change2 = change.ToArray();
 
 
+List<char> numbers = new List<char>();
+numbers.Add('0');
+numbers.Add('1');
+numbers.Add('2');
+numbers.Add('3');
+numbers.Add('4');
+numbers.Add('5');
+numbers.Add('6');
+numbers.Add('7');
+numbers.Add('8');
+numbers.Add('9');
+
+Console.Write("Wynik dla liczby " + number + "\n");
+
+foreach (var search in numbers)
+{
+    int zmienna = 0;
+
+    foreach (char c in change2)
+    {
+        if (c == search)
+        {
+            zmienna++;
+        }
+    }
+
+    //Console.WriteLine("Liczba {0} występuje {1} razy.", search, licz);
+
+    string x = "raz";
+
+    if (zmienna != 1)
+    {
+
+        Console.WriteLine(search + " występuje => " + zmienna + " " + x + "y");
+    }
+    else
+    {
+        Console.WriteLine(search + " występuje => " + zmienna + " " + x);
+    }
+}
+
+
+//int number = 1712154;
+//string change = number.ToString();
+//char[] change2 = change.ToArray();
+
+
+//List<char> numbers = new List<char>();
+//numbers.Add('0');
+//numbers.Add('1');
+//numbers.Add('2');
+//numbers.Add('3');
+//numbers.Add('4');
+//numbers.Add('5');
+//numbers.Add('6');
+//numbers.Add('7');
+//numbers.Add('8');
+//numbers.Add('9');
+
+//Console.Write("Wynik dla liczby " + number + "\n");
+
+//string[] teksty = { "zero", "jeden", "dwa", "trzy", "cztery", "pięć", "sześć", "siedem", "osiem", "dziewięć" };
+
+//foreach (var search in numbers)
+//{
+//    int zmienna = 0;
+//    foreach (char c in change2)
+//    {
+//        if (c == search)
+//        {
+//            zmienna++;
+//        }
+//    }
+
+//    //string x = "raz";
+//    string text = zmienna.ToString();
+//    if (zmienna != 1)
+//    {
+//        Console.WriteLine(teksty[int.Parse(search.ToString())] + " występuje => " + text );
+//    }
+//    else
+//    {
+//        Console.WriteLine(teksty[int.Parse(search.ToString())] + " występuje => " + text );
+//    }
+//}
